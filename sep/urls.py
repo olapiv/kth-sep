d1305324c50116c2f39be64c9162fdd1aff24a6c
Vincent Lohse <vincent.lohse@live.de>
2019-10-01 18:41:02 +0200

    Fixed urls

--- sep/urls.py
+++ sep/urls.py
@@ -5,18 +5,17 @@ from django.contrib.auth.decorators import login_required
 from django.contrib.admin.views.decorators import staff_member_required
 from sep import views
 
-login_required(login_required(
 
 urlpatterns = [
-    path('applications/create', login_required(login_required(views.EventRequestApplicationCreate.as_view())), name='create_application'),
-    path('applications', login_required(login_required(views.EventRequestApplicationList.as_view())), name="list_applications"),
-    path('applications/<int:pk>/', login_required(login_required(views.EventRequestApplicationUpdate.as_view())), name="update_application"),
+    path('applications/create', staff_member_required(login_required(views.EventRequestApplicationCreate.as_view())), name='create_application'),
+    path('applications', staff_member_required(login_required(views.EventRequestApplicationList.as_view())), name="list_applications"),
+    path('applications/<int:pk>/', staff_member_required(login_required(views.EventRequestApplicationUpdate.as_view())), name="update_application"),
 
-    path('tasks/create', login_required(login_required(views.SubteamTaskCreate.as_view())), name='create_task'),
-    path('tasks', login_required(login_required(views.SubteamTaskList.as_view())), name='list_tasks'),
-    path('tasks/<int:pk>', login_required(login_required(views.SubteamTaskUpdate.as_view())), name='update_task'),
+    path('tasks/create', staff_member_required(login_required(views.SubteamTaskCreate.as_view())), name='create_task'),
+    path('tasks', staff_member_required(login_required(views.SubteamTaskList.as_view())), name='list_tasks'),
+    path('tasks/<int:pk>', staff_member_required(login_required(views.SubteamTaskUpdate.as_view())), name='update_task'),
 
-    path('budget-requests/create', login_required(login_required(views.ExtraBudgetRequestCreate.as_view())), name='create_budget_request'),
-    path('budget-requests', login_required(login_required(views.ExtraBudgetRequestList.as_view())), name='list_budget_request'),
-    path('budget-requests/<int:pk>', login_required(login_required(views.ExtraBudgetRequestUpdate.as_view())), name='update_budget_request'),
+    path('budget-requests/create', staff_member_required(login_required(views.ExtraBudgetRequestCreate.as_view())), name='create_budget_request'),
+    path('budget-requests', staff_member_required(login_required(views.ExtraBudgetRequestList.as_view())), name='list_budget_request'),
+    path('budget-requests/<int:pk>', staff_member_required(login_required(views.ExtraBudgetRequestUpdate.as_view())), name='update_budget_request'),
 ]
