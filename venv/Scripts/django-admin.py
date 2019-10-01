e3809c0835a7d48433b8760f2917df28f8354d2a
Vincent Lohse <vincent.lohse@live.de>
2019-10-01 15:21:20 +0200

    Deleted venv

--- venv/Scripts/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!C:\Users\minda\Programs\KTH\Modern Methods of Software Engineering\Project\venv\Scripts\python.exe
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
